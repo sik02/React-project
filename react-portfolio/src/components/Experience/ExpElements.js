4700583e2672107e73eda09fb21ae1c4d29d19cb
KimJunSik <pkwaw@naver.com>
2022-08-25 18:41:05 +0900

    experience 구성

--- react-portfolio/src/components/Experience/ExpElements.js
+++ react-portfolio/src/components/Experience/ExpElements.js
@@ -3,3 +3,94 @@ import styled from 'styled-components';
 export const ExpSection = styled.section`
 
 `
+
+export const H5Text = styled.h5`
+
+`
+
+export const H2Text = styled.h2`
+
+`
+
+export const H3Text = styled.h3`
+    text-align: center;
+    margin: 0 0 2rem 0;
+    color: #4db5ff;
+`
+
+export const H4Text = styled.h4`
+
+`
+
+export const SmallText = styled.small`
+
+`
+
+export const ExpContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 2rem;
+    width: 75%;
+    margin: 0 auto;
+
+    @media screen and (max-width: 1024px){
+        grid-template-columns: 1fr;
+    }
+
+    @media screen and (max-width: 600px){
+        gap: 1rem;
+    }
+
+`
+
+export const Content = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 2rem;
+
+    @media screen and (max-width: 1024px){
+        padding: 1rem;
+    }
+`
+
+export const Container = styled.div`
+    background: #2c2c6c;
+    padding: 2.4rem 5rem;
+    border-radius: 2rem;
+    border: 1px solid transparent;
+    transition: all 400ms ease;
+
+    &:hover{
+        background: transparent;
+        border-color: rgba(77,181,255,0.4);
+        cursor: default;
+    }
+
+    @media screen and (max-width: 1024px){
+        width: 80%;
+        padding: 2rem;
+        margin: 0 auto;
+    }
+
+    @media screen and (max-width: 600px){
+        width: 100%:
+        padding: 2rem 1rem;
+    }
+
+`
+
+
+export const Article_Detail = styled.article`
+    display: flex;
+    gap: 1rem;
+`
+
+export const Detail = styled.div`
+
+`
+
+export const IconBox = styled.div`
+    margin-top: 6px 0 0 0;
+    color: #4db5ff;
+`
+
--- react-portfolio/src/components/Experience/Experience.js
+++ react-portfolio/src/components/Experience/Experience.js
@@ -1,13 +1,136 @@
 import React from 'react'
 import {
   ExpSection,
+  H5Text,
+  H2Text,
+  H3Text,
+  H4Text,
+  SmallText,
+  ExpContainer,
+  Container,
+  Content,
+  Article_Detail,
+  Detail,
+  IconBox,
 } from './ExpElements';
+
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Experience = () => {
   return (
     <>
       <ExpSection id='experience'>
-        Experience
+        <H5Text>What</H5Text>
+        <H2Text>My Experience</H2Text>
+        <ExpContainer>
+          <Container>
+            <H3Text>Frontend Development</H3Text>
+            <Content>
+
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>HTML</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>CSS</H4Text>
+                  <SmallText>Intermediate</SmallText>
+                </Detail>
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>JavaScript</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>Bootstrap</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>Tailwind</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>React</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+
+
+            </Content>
+          </Container>
+
+          <Container>
+            <H3Text>Backend Development</H3Text>
+            <Content>
+
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>Node JS</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>MongoDB</H4Text>
+                  <SmallText>Intermediate</SmallText>
+                </Detail>
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>PHP</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>MySQL</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+
+              </Article_Detail>
+              <Article_Detail>
+                <IconBox><BsPatchCheckFill /></IconBox>
+                <Detail>
+                  <H4Text>Python</H4Text>
+                  <SmallText>Experienced</SmallText>
+                </Detail>
+
+              </Article_Detail>
+
+
+            </Content>
+          </Container>
+        </ExpContainer>
       </ExpSection>
     </>
   )
