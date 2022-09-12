e5409cd0390272075e478ec4d9d525b1639a29ac
KimJunSik <pkwaw@naver.com>
2022-09-13 05:15:28 +0900

    헤더 구성 완료

--- react-gatsby-blog/src/components/layout.js
+++ react-gatsby-blog/src/components/layout.js
@@ -1,16 +1,11 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
 import Header from "../components/Header/Header"
+import { GlobalStyle } from "./styles/GlobalStyles";
 
 const Layout = ({ children }) => {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <main>{children}</main>
     </>
