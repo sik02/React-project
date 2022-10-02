1b29640d4675ffad34f98c08746d8e4468e4dae9
KimJunSik <pkwaw@naver.com>
2022-10-02 23:28:09 +0900

    google search api 오류 해결중

--- google-search/src/components/Results.jsx
+++ google-search/src/components/Results.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import Loading from './Loading';
+import { useResultContext } from '../contexts/ResultContextProvider';
 
 const Results = () => {
-  return (
-    <div>Results</div>
-  )
+  const { results, isLoading, getResults, searchTerm } = useResultContext();
+  const location = useLocation();
+
+  useEffect(() => {
+    getResults('/search?q=JavaScript Mastery&num=40')
+    // https://www.google.com/news/search?q=abc
+    // getResults('')
+  }, []);
+
+  if(isLoading){
+    return <Loading />
+  }
+
+  switch(location.pathname){
+    case '/search':
+      return (
+        <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+          {/* {results?.results?.map(({ link, title }) => (
+            
+          ))} */}
+        </div>
+      )
+    case '/images':
+      return 'SEARCH';
+    default:
+      return 'ERROR!';
+  }
 }
 
 export default Results
--- google-search/src/contexts/ResultContextProvider.js
+++ google-search/src/contexts/ResultContextProvider.js
@@ -1,4 +1,6 @@
-import React, { createContext, useContext, useState } from "React";
+import React, { createContext, useContext, useState } from "react";
+
+
 
 const ResultContext = createContext();
 const baseUrl = 'https://google-search-5.p.rapidapi.com/google/organic-search';
@@ -7,20 +9,31 @@ export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [searchTerm, setSearchTerm] = useState('');
-
+    
     const getResults = async (type) => {
         setIsLoading(true);
 
         const response = await fetch(`${baseUrl}${type}`, {
             method: 'POST',
             headers: {
-                'content-type': 'application/json',
-                'X-RapidAPI-Key': '36dd2fe836mshb5f0d3d08bb744fp188682jsn0f9c72f43fb2',
-                'X-RapidAPI-Host': 'google-search-5.p.rapidapi.com'
-            }
+              'content-type': 'application/json',
+              'X-RapidAPI-Key': '36dd2fe836mshb5f0d3d08bb744fp188682jsn0f9c72f43fb2',
+              'X-RapidAPI-Host': 'google-search-5.p.rapidapi.com'
+            },
+            // data: '{"query":"abc","gl":"US","hl":"en_US","device":"desktop","duration":"","autocorrect":0,"page":1,"uule":"abc","pages":1}'
         });
 
-        const data = await response.json();
-        setResults(data);
+        const data2 = await response.json();
+        console.log(data2);
+        // setResults(data);
+        setIsLoading(false);
     }
+
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, isLoading }}>
+            {children}
+        </ResultContext.Provider>
+    )
 }
+
+export const useResultContext = () => useContext(ResultContext);
