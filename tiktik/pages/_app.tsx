1a317907f7896131dcb9e41a7d23415acd36bc37
KimJunSik <pkwaw@naver.com>
2022-10-04 04:12:23 +0900

    데탑 환경 구축, app_tsx 추가중

--- tiktik/pages/_app.tsx
+++ tiktik/pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setisSSR] = useState(true);
+
+  useEffect(() => {
+    setisSSR(false);
+  }, []);
+
+  if (isSSR) {
+    return null;
+  }
+
+  return (
+    <div>
+      Navbar
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          Sidebar
+        </div>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
